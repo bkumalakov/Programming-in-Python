e7f79a572b3d295ab8fdfa5bd2c744da6eb4d180
Bolatzhan <b.kumalakov@gmail.com>
2020-11-02 08:51:42 +0500

    Assignment task 4

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,7 +1,9 @@
 # глобальные переменные
 userChoice = 0      # переменная, которая хранит выбор пользователя
-                    # Задание 1: вобъявить переменную, котоая будет хранить 15 значений
+values = []         # Задание 1: объявить переменную, котоая будет хранить 15 значений
 
+
+# Выводим меню пользователя
 print('Меню:')
 print('1. Вывести на экран все знаения')
 print('2. Добавить значение')
@@ -10,21 +12,69 @@ print('4. Найти значение')
 print('5. Отсортировать значения')
 print('6. Выйти')
 print('Введите опцию:')
+
+# Считываем ввод пользователя с клавиатуры
 userChoice = int(input())
+
 
 # Задание 2: в цикле while создать ограничения для ввода опций таким образом, чтобы
 # программа принимала только знаенчия, из меню (1-6), в остальных случаях выдвала ошибку.
-
-# Задание 3: Реализовать опцию 1 и 2 из списка меню.
 while userChoice != 6:
-    print('Введите опцию:', userChoice)
+    if (userChoice < 1) | (userChoice > 6):                     # проверить, что выбор пользоателя в диапазоне 1-6
+        print('Такой опции нет, выберите существующую опцию.')
+        userChoice = int(input())
+    else:
+        # Задание 3: распечатать значения.
+        if userChoice == 1:
+            print(values)
+        # Задание 3: добавить новое значение.
+        elif userChoice == 2:
+            print('Введите новое число:')
+            newValue = int(input())
+            values.append(newValue)
+        # Задание 4: реализовать удаление значений путем перезаписи идей справа на лево.
+        elif userChoice == 3:
+            if len(values) != 0:
+                print('Введите число для удаления:')
+                searchValue = int(input())
+                counter = 0
+                deleted = False
+                for count in range(0, len(values)):
+                    if (values[count] == searchValue) & (count < len(values)):
+                        deleted = True
+                        while count+1 < len(values):
+                            values[count] = values[count+1]
+                            count = count+1
+                if deleted is True:
+                    del values[count]
+                elif deleted is False:
+                    print('Значения нет в базе данных.')
+            else:
+                print('База пустая! Добавте значения!')
+        # Задание 4: реализовать поиск значения с выводом его индекса в списке.
+        elif userChoice == 4:
+            if len(values) != 0:
+                print('Введите число для поиска его индекса:')
+                searchValue = int(input())
+                found = False
+                for count in range(0, len(values)):
+                    if values[count] == searchValue:
+                        found = True
+                        print('Индекс значения "', searchValue, '":', count)
+                if found is False:
+                    print('Значение не найдено!')
+            else:
+                print('База пустая! Не можем искать!')
+        elif userChoice == 5:
+            print('Oпция ', userChoice)
+        print('\n Меню:')
+        print('1. Вывести на экран все знаения')
+        print('2. Добавить значение')
+        print('3. Удалить значение')
+        print('4. Найти значение')
+        print('5. Отсортировать значения')
+        print('6. Выйти')
+        print('Введите опцию:')
+        userChoice = int(input())
 
-    print('Меню:')
-    print('1. Вывести на экран все знаения')
-    print('2. Добавить значение')
-    print('3. Удалить значение')
-    print('4. Найти значение')
-    print('5. Отсортировать значения')
-    print('6. Выйти')
-    print('Введите опцию:')
-    userChoice = int(input())
+

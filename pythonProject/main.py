029c4520b247f5107de0079fb34196f7d09550f3
Bolatzhan <b.kumalakov@gmail.com>
2020-11-24 17:13:45 +0500

    another commit

---
+++ pythonProject/main.py
@@ -0,0 +1,12 @@
+# функция print()
+#   print(1)    #распечатает цифру 1 на экране
+
+ # print(`Hello world`)
+
+ # пременная
+a = 12
+b = int(input())
+print('a+b =',  a+b)
+
+ # input()
+ # x = input()
